b62dabb9d69aa6da0e66f2c61fd2a20558b77fda
hd321kbps <hd321kbps@yandex.ru>
2023-02-03 13:39:04 +0300

    bump version

--- updater.js
+++ updater.js
@@ -2,44 +2,44 @@
 // const { dialog } = require('electron');
 
 const checkForUpdates = () => {
-  console.log('Checking for updates');
+  // console.log('Checking for updates');
   autoUpdater.logger = require('electron-log');
   autoUpdater.logger.transports.file.level = 'info';
   autoUpdater.autoDownload = false;
-  // autoUpdater.checkForUpdates();
+  autoUpdater.checkForUpdates();
 
-  // autoUpdater.on('update-available', () => {
-  //   dialog
-  //     .showMessageBox({
-  //       type: 'info',
-  //       title: 'Найдено обновление',
-  //       message: 'Вышла новая версия приложения. Скачать и установить?',
-  //       buttons: ['Да', 'Нет']
-  //     })
-  //     .then(({ response }) => {
-  //       if (response === 0) {
-  //         autoUpdater.downloadUpdate();
-  //       }
-  //     });
-  // });
+  autoUpdater.on('update-available', () => {
+    dialog
+      .showMessageBox({
+        type: 'info',
+        title: 'Найдено обновление',
+        message: 'Вышла новая версия приложения. Скачать и установить?',
+        buttons: ['Да', 'Нет']
+      })
+      .then(({ response }) => {
+        if (response === 0) {
+          autoUpdater.downloadUpdate();
+        }
+      });
+  });
 
-  // autoUpdater.on('update-downloaded', () => {
-  //   dialog
-  //     .showMessageBox({
-  //       type: 'info',
-  //       title: 'Обновление готово',
-  //       message: 'Установить и перезагрузить приложение?',
-  //       buttons: ['Да', 'Позже']
-  //     })
-  //     .then(({ response }) => {
-  //       if (response === 0) {
-  //         autoUpdater.quitAndInstall(false, true);
-  //       }
-  //     });
-  // });
+  autoUpdater.on('update-downloaded', () => {
+    dialog
+      .showMessageBox({
+        type: 'info',
+        title: 'Обновление готово',
+        message: 'Установить и перезагрузить приложение?',
+        buttons: ['Да', 'Позже']
+      })
+      .then(({ response }) => {
+        if (response === 0) {
+          autoUpdater.quitAndInstall(false, true);
+        }
+      });
+  });
 
   // Проверка и автоматическое скачивание с уведомлением
-  autoUpdater.checkForUpdatesAndNotify();
+  // autoUpdater.checkForUpdatesAndNotify();
 };
 
 module.exports = { checkForUpdates };
